02002ceeb3928d3aa863f827c3b66a8443e9a0db
Nico Andrade <nico@nmac.com.ar>
2021-01-11 20:38:36 -0500

    cleanup

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -16,29 +16,19 @@ class Contact extends Component {
 
     return (
       <section id="contact">
-
          <div className="row section-head">
-
             <div className="two columns header-col">
-
                <h1><span>Get In Touch.</span></h1>
-
             </div>
-
             <div className="ten columns">
-
                   <p className="lead">{message}</p>
-
             </div>
-
          </div>
 
          <div className="row">
             <div className="eight columns">
-
                <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
-
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
@@ -68,49 +58,24 @@ class Contact extends Component {
 					</fieldset>
 				   </form>
 
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
+               <div id="message-warning"> Error boy</div>
+                     <div id="message-success">
+                        <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                     </div>
+               </div>
 
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
-      </div>
-   </section>
+               <aside className="four columns footer-widgets">
+                  <div className="widget widget_contact">
+                     <h4>Address and Phone</h4>
+                     <p className="address">
+                        {name}<br />
+                        {city}, {state} {zip}<br />
+                        <span>{phone}</span>
+                     </p>
+                  </div>
+               </aside>
+         </div>
+      </section>
     );
   }
 }
