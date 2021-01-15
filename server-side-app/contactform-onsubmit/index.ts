69488e78b951c696db47b9991682033e32bf1371
Nico Andrade <nico@nmac.com.ar>
2021-01-15 15:08:02 -0500

    enable load environment variables

--- server-side-app/contactform-onsubmit/index.ts
+++ server-side-app/contactform-onsubmit/index.ts
@@ -1,11 +1,12 @@
+require('dotenv').config()
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
+    const responseMessage = (name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.") + "And the Environment:" + JSON.stringify(process.env);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
