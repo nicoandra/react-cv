0395100ad49fa64cc7127598e1d4ab1fb40930da
Nico Andrade <nico@nmac.com.ar>
2021-01-17 15:59:10 -0500

    cleanup prod version

--- ui-cv/src/Components/About.js
+++ ui-cv/src/Components/About.js
@@ -3,16 +3,8 @@ import PropTypes from "prop-types";
 class About extends Component {
 	render() {
 		if (this.props.data) {
-			var name = this.props.data.name;
 			var profilepic = "images/" + this.props.data.image;
 			var bio = this.props.data.bio;
-			var street = this.props.data.address.street;
-			var city = this.props.data.address.city;
-			var state = this.props.data.address.state;
-			var zip = this.props.data.address.zip;
-			var phone = this.props.data.phone;
-			var email = this.props.data.email;
-			var resumeDownload = this.props.data.resumedownload;
 		}
 
 		return (
@@ -29,32 +21,7 @@ class About extends Component {
 						<h2>About Me</h2>
 
 						<p>{bio}</p>
-						<div className="row">
-							<div className="columns contact-details">
-								<h2>Contact Details</h2>
-								<p className="address">
-									<span>{name}</span>
-									<br />
-									<span>
-										{street}
-										<br />
-										{city} {state}, {zip}
-									</span>
-									<br />
-									<span>{phone}</span>
-									<br />
-									<span>{email}</span>
-								</p>
-							</div>
-							<div className="columns download">
-								<p>
-									<a href={resumeDownload} className="button">
-										<i className="fa fa-download"></i>
-										Download Resume
-									</a>
-								</p>
-							</div>
-						</div>
+
 					</div>
 				</div>
 			</section>
