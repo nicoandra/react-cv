3c8fcb7e89be898a376151e6e72fec2f43ebf0c9
Nico Andrade <nico@nmac.com.ar>
2022-03-08 14:09:20 -0500

    cleanup

--- ui-cv/src/Components/About.js
+++ ui-cv/src/Components/About.js
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-class About extends Component {
-	render() {
-		if (this.props.data) {
-			var name = this.props.data.name;
-			var profilepic = "images/" + this.props.data.image;
-			var bio = this.props.data.bio;
-			var city = this.props.data.address.city;
-			var state = this.props.data.address.state;
-			var zip = this.props.data.address.zip;
-			var linkedInUrl = this.props.data.linkedInUrl
-			var resumeDownload = this.props.data.resumedownload;
-		}
-=======
 import React from "react";
->>>>>>> main
 
 function About({data, columnSizes}) {
 
@@ -27,10 +10,8 @@ function About({data, columnSizes}) {
 	const bio = data.bio;
 	const street = data.address.street;
 	const { city, state, zip } = data.address;
-	const phone = data.phone;
-	const email = data.email;
-	const resumeUrl = data.resumeUrl;
-	
+	const resumeDownloadUrl = data.resumeDownloadUrl || '';
+	const linkedInUrl = data.linkedInUrl
 	return (
 		<section id="about">
 			<div className="row">
@@ -48,26 +29,19 @@ function About({data, columnSizes}) {
 						<div className="columns contact-details screen">
 							<h2>Contact Details</h2>
 							<p className="address">
-								<span>{name}</span>
-								<br />
-								<span>
-									{street}
-									<br />
-									<span>
-										{city} {state}, {zip}
-									</span>
-									<br />
-									<span><a href={linkedInUrl} target="_blank" rel="noreferrer">LinkedIn Profile</a></span>
-								</p>
-							</div>
-							<div className="columns download">
-								<p>
-									<a href={resumeDownload} className="button">
-										<i className="fa fa-download"></i>
-										Download Resume
-									</a>
-								</p>
-							</div>
+								<span>{name}</span><br />
+								<span>{street}</span><br />
+								<span>{city} {state}, {zip}</span><br />
+								<span><a href={linkedInUrl} target="_blank" rel="noreferrer">LinkedIn Profile</a></span>
+							</p>
+						</div>
+						<div className="columns download">
+							<p>
+								<a href={resumeDownloadUrl} className="button">
+									<i className="fa fa-download"></i>
+									Download Resume
+								</a>
+							</p>
 						</div>
 					</div>
 				</div>
